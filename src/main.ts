4d72fbab411ceee6ba3bfb54117f2b8ec85cb63d
pav <pav@Mr.net>
2022-04-06 13:58:13 +0200

    initial checkin

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './styles/style.scss';
+import './styles/bulmaImports.scss';
+import './styles/parallax.scss';
+import './styles/header.scss';
+
+import './scripts/header.ts'
---
+++ src/scripts/header.ts
@@ -0,0 +1,15 @@
+let header: HTMLHeadElement;
+
+(function(){
+    document.addEventListener("scroll", documentScrolled);
+    header = document.getElementsByTagName("header")[0] as HTMLHeadElement;
+})();
+
+function documentScrolled(){ 
+    const topOffset = window.scrollY;
+    console.log(topOffset);
+    if(topOffset === 0)
+        header.classList.add("pav-is-top");
+    else 
+        header.classList.remove("pav-is-top");
+};
